c6dae507c7a524f3f35448f1f473408327534e6d
KyleJKC <kj@kylejin.com>
2025-10-26 13:25:21 -0700

    optimize fish performance

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -36,11 +36,24 @@ if ! set -q lucid_git_icon
     set -g lucid_git_icon ""
 end
 
+if ! set -q lucid_ssh_icon
+    set -g lucid_ssh_icon "🌐"
+end
+
+if ! set -q lucid_ssh_color
+    set -g lucid_ssh_color yellow
+end
+
+# Skip newline on first prompt
+set -g lucid_skip_newline 1
+
 # State used for memoization and async calls.
 set -g __lucid_cmd_id 0
 set -g __lucid_git_state_cmd_id -1
 set -g __lucid_git_static ""
 set -g __lucid_dirty ""
+set -g __lucid_last_prompt_cmd_id -1
+set -g __lucid_should_skip_newline 0
 
 # Increment a counter each time a prompt is about to be displayed.
 # Enables us to distingish between redraw requests and new prompts.
@@ -207,12 +220,37 @@ end
 function fish_mode_prompt
 end
 
+# Wrapper functions to skip newline after clearing screen
+function clear --description 'Clear the screen and skip newline on next prompt'
+    set -g lucid_skip_newline 1
+    command clear $argv
+end
+
+function reset --description 'Reset the terminal and skip newline on next prompt'
+    set -g lucid_skip_newline 1
+    command reset $argv
+end
+
 function fish_prompt
     set -l last_pipestatus $pipestatus
     # OPTIMIZED: Use prompt_pwd which is faster and handles ~ replacement
     set -l cwd (prompt_pwd)
 
-    if test -z "$lucid_skip_newline"
+    # On a new prompt (not a repaint), decide whether to skip the newline
+    if test $__lucid_cmd_id -ne $__lucid_last_prompt_cmd_id
+        set -g __lucid_last_prompt_cmd_id $__lucid_cmd_id
+
+        # Store the decision: should we skip newline for this prompt?
+        if set -q lucid_skip_newline
+            set -g __lucid_should_skip_newline 1
+            set -e lucid_skip_newline
+        else
+            set -g __lucid_should_skip_newline 0
+        end
+    end
+
+    # Apply the decision on every render (including repaints)
+    if test $__lucid_should_skip_newline -eq 0
         echo ''
     end
 
@@ -225,6 +263,21 @@ function fish_prompt
         if test $status -eq 0
             echo -sn " on $git_state"
         end
+    end
+
+    # Display SSH info on the right side of the first line
+    if set -q SSH_CONNECTION
+        set -l ssh_info "$lucid_ssh_icon $USER@"(hostname -s)
+        # Calculate length of visible characters (strip ANSI codes for length calculation)
+        set -l ssh_info_length (string length -- $ssh_info)
+        # Move cursor to the right position and print
+        # Use tput to move cursor: save position, move to column, restore
+        echo -n (tput sc) # Save cursor position
+        echo -n (tput hpa (math $COLUMNS - $ssh_info_length)) # Move to calculated column
+        set_color $lucid_ssh_color
+        echo -n $ssh_info
+        set_color normal
+        echo -n (tput rc) # Restore cursor position
     end
 
     echo ''
